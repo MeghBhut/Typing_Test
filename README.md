b07cf4a91f2151af897f8e25f499c93a70cb4990
MeghBhut <meghbhut90@gmail.com>
2025-10-19 14:29:41 +0530

    Releasing version 3
    This adds color codes for correct and missed charcahters plus backspace support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Typing Speed and Accuracy Test version 2
 
-This is a simple console-based typing test program written in C. It generates a random paragraph by combining sentences read from a text file. Users type the paragraph as quickly and accurately as possible. The program then calculates typing speed (words per minute) and accuracy percentage.
+This is a simple console-based typing test program written in C. It generates a random paragraph by combining sentences read from a text file. Users type the paragraph as quickly and accurately as possible. The program then calculates typing speed (words per minute) and accuracy percentage also shows how many and which charachter is missed typed
 
 ---
 
@@ -10,6 +10,7 @@ This is a simple console-based typing test program written in C. It generates a 
 - Allows user to select the number of sentences for the test
 - Measures typing speed in Words Per Minute (WPM)
 - Calculates typing accuracy
+- Shows which charachter is missed typed
 - Handles input with spaces, tabs, and newlines gracefully
 
 ---
@@ -23,15 +24,15 @@ This is a simple console-based typing test program written in C. It generates a 
 
 ## How to Compile and Run
 
-1. Open a terminal or command prompt in your project directory.
+1. Open a terminal in your project directory.(Run this in VS Code Terminal, Git Bash, or Windows Terminal for best color support.)
 2. Compile the program using GCC:
 
    ```bash
-   gcc typing_test_V2.c -o typing_test
+   gcc typing_test_V3.c -o typing_test
 3. Make sure your `sentences.txt` file is in the same directory as the executable.
 4. Run the program:
     ```bash
-    ./typing_test_V2
+    ./typing_test_V3
 
 ---
 
@@ -53,14 +54,20 @@ This is a simple console-based typing test program written in C. It generates a 
     ```bash
     Give how many sentences type test want to do: 2
     Type given Para fast:
-    The wizards vault hid five quirky boxing jackals Queen Victorias jester jumped boldly on hazy fox trails
+    Jumpy Fred boxed a dozen hazy jugs for the wizard queen Zippy wizards help Jack fix broken quaint magic boxes
     Press any key to Start: 
     Your input here...
     =================== Result ===================
-    Words is: 17
+    Words is: 20
     Time is: 33.00 sec
-    Your speed is: 30.91 WPM
-    Your accuracy is: 87.50 %
+    Your speed is: 36.36 WPM
+    Your accuracy is: 95.41 %
+    Your Mistakes:
+        "J": 1
+        "e": 1
+        "m": 1
+        "u": 1
+        "z": 1
     
     ==============================================
 
@@ -69,12 +76,12 @@ This is a simple console-based typing test program written in C. It generates a 
 ## Notes and limitations
 - Input paragraph length limited to 1000 characters, extend it if you want.
 - Sentences read up to 200 lines from the input file, extend it if you want
-- It dont show which wrong charachter is typed.
+- It has only one type of typing test mode
 
 ---
 
-## Version 2 changes
-- Now it measure charachter lively accurate accuracy
+## Version 3 changes
+- Now it measure charachter lively accurate accuracy and red color for wrong and green for right entry also backspace support
 - Probelm of accedentl new line charachter or missed press enter finish test is solvevd
 
 ---
--- Type_test_V3.c
+++ Type_test_V3.c
@@ -9,29 +9,29 @@
  * a random paragraph composed of user-specified number of sentences.
  * The user types the paragraph, and the program calculates
  * typing speed in words per minute (WPM) and accuracy percentage.
+ * shows missed typed words
  *
  * Usage:
- * Compile: gcc typing_test_V2.c -o typing_test
- * Run: ./typing_test_V2
+ * Compile: gcc typing_test_V3.c -o typing_test
+ * Run: ./typing_test_V3
  *
  * Inputs:
  * - Number of sentences to include in the typing test
- * - User's typed paragraph
+ * - User's typed paragraph geen text is corrected and red will be wrong
  *
  * Outputs:
  * - Words typed
  * - Time taken
  * - Typing speed (WPM)
  * - Accuracy percentage
+ * - Which charachter is miss typed
  *
  * Limitations:
  * - Input paragraph length limited to 1000 characters, extend it if you want.
  * - Sentences read up to 200 lines from the input file, extend it if you want
- * - Can be somethimes incompelte as it dont measure live typed word some mistakes
- *   while writing paragraph not count in mistakes while accuracy checking.
- * - Other thing if some letter is missed typed double times then whole part
- *   after that will be given wrong.
+ * - Only one mode of typing test
  */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -43,6 +43,9 @@
 # define MAX_PARAGRAPH_LENGTH 1000
 # define MAX_LINE_LENGTH 300
 # define MAX_USER_INPUT 1000
+# define RED     "\033[1;31m"
+# define GREEN   "\033[1;32m"
+# define RESET   "\033[0m"
 
 int main(){
     // declaring file
@@ -87,31 +90,45 @@ int main(){
     printf("Type given Para fast: \n");
     printf("%s",paragraph); // givining on screen para
     printf("\nPress any key to Start: \n");
-    getch();
+    getch(); // buffer clear
     int typed_char=0,mistakes=0;
-    time_t start_time = time(NULL);
+    int mistake_freq[256]={0}; // defining misstake freq here unsiged charachter so 1-256 represent charchters
+    time_t start_time = time(NULL); //starting time
     while (1)
     {
-        char user_input = getch();
-        if (user_input==paragraph[typed_char])
+        char user_input = getch(); //taking input
+
+        if (user_input == 8 || user_input == 127) // here 8 and 127 are backspaces
         {
-            printf("%c",user_input);
-            typed_char++;
+            if (typed_char>0)
+            {
+                typed_char--;
+                printf("\b \b"); // \b shifts cursor back ' ' will erase charchter and \b again to back cursor
+            }
+            continue; // skipping loop
+        }
+        if (user_input==paragraph[typed_char]) // if input matched paragraph
+        {
+            printf(GREEN"%c"RESET,user_input); // print it with green color
         }
         else
         {
-            mistakes++;
+            printf(RED"%c"RESET,user_input); // else print it with red coloe
+            mistakes++; // adding mistake
+            mistake_freq[(unsigned char)paragraph[typed_char]]++; // increasing mistake freq by one.
         }
-        if (typed_char==strlen(paragraph))
+        typed_char++;
+        if (typed_char==strlen(paragraph)) // if paragraph finish break loop
         {
             break;
         }
     }
-    time_t end_time = time(NULL);
+    time_t end_time = time(NULL); // ending time
     
-    double time_taken_min = (double)difftime(end_time,start_time)/60;
-    double accuracy = ((double)(typed_char-mistakes)/typed_char)*100;
+    double time_taken_min = (double)difftime(end_time,start_time)/60; // taking duration of time
+    double accuracy = ((double)(typed_char-mistakes)/typed_char)*100; // measuring accuracy
     
+    // checking no of words in this paragraph
     int is_inside_word=0,No_of_Words=0;
     for (int i = 0; i < strlen(paragraph); i++)
     {
@@ -125,6 +142,7 @@ int main(){
             No_of_Words++;
         }
     }
+    // measuring speed
     double speed  = (double)No_of_Words/time_taken_min;
     //printing final results
     printf("\n=================== Result ===================\n");
@@ -132,6 +150,25 @@ int main(){
     printf("Total time taken is: %.2f sec \n",time_taken_min*60);
     printf("Your speed is: %.2f WPM\n",speed);
     printf("Your accuracy is: %.2f %% \n",accuracy);
+    printf("Your Mistakes: \n");
+    for (int i = 0; i < 256; i++)
+    {
+        if (mistake_freq[i]>0)
+        {
+            if (i ==' ')
+            {
+                printf("\tspaces(' '): %d\n",mistake_freq[i]);
+            }
+            else if(i== '\n')
+            {
+                printf("\tnewline(Enter): %d\n",mistake_freq[i]); 
+            }
+            else
+            {
+                printf("\t\"%c\": %d\n",i,mistake_freq[i]);
+            }
+        }
+    }
     printf("\n==============================================\n");
 
     // freeing space
